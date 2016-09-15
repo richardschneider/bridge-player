4197f357b76bdd783965fc69d03d20bd339f591b
Richard Schneider <makaretu@gmail.com>
2016-09-15 14:39:31 +1200

    feat(player): has a seat and team name

--- test/player.js
+++ test/player.js
@@ -2,16 +2,33 @@
 
 require('should');
 
-let Player = require('..');
+let Player = require('..'),
+    Table = require('./table'),
+    bridge = require('bridge.js'),
+    seat = bridge.seat;
+
 
 describe('Bridge Player', () => {
 
-    it('should be seated', done => {
-        done('NYI');
+    it('should be seated', () => {
+        let player = new Player();
+        player.should.have.property('seat', seat.south);
+
+        player = new Player({seat: seat.north});
+        player.should.have.property('seat', seat.north);
     });
 
-    it('should be a member of a team', done => {
-        done('NYI');
+    it('should be a team member', () => {
+        let player = new Player();
+        player.should.have.property('teamName', 'emanon');
+
+        player = new Player({ teamName: 'Blue Team' });
+        player.should.have.property('teamName', 'Blue Team');
     });
+
+    it('should exchange messages with the table', () => {
+        let player = new Player();
+        player.connect(new Table());
+     });
 
 });
---
+++ test/table.js
@@ -0,0 +1,5 @@
+function TestTable() {
+
+}
+
+module.exports = TestTable;
