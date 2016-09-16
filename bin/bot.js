8bd5e0b0ba80b57ae36f7aad59075334ac579f26
Richard Schneider <makaretu@gmail.com>
2016-09-16 16:49:40 +1200

    feat(bot): the basics for a robot that can play bridge

---
+++ bin/bot.js
@@ -0,0 +1,58 @@
+#! /usr/bin/env node
+
+'use strict';
+
+var program = require('commander');
+var bridge = require('bridge.js');
+var Player = require('../index');
+var net = require('net');
+var process = require('process');
+
+program
+    .usage('[options]')
+    .description('A robot that plays bridge')
+    .option('-d, --debug', 'show the table messages')
+    .option('-i, --ignore', 'ignore errors and keep on playing')
+    .option('-n, --number [#]', 'number of bots to make')
+    .option('-s, --seat [seat]', 'the seat to play, defaults to north')
+    .option('-t, --team [name]', 'team name')
+    .option('-p, --port [number]', 'defaults to 2000')
+    .option('-h, --host [name]', 'defaults to localhost')
+    ;
+
+program.parse(process.argv);
+
+let seat = bridge.seat[program.seat || 'south'],
+    team = program.team,
+    botCount = parseInt(program.number) || 1,
+    port = parseInt(program.port) || 2000,
+    host = program.host || 'localhost';
+
+function makeBot(seat) {
+    let me = new Player({
+        seat: seat,
+        teamName: team
+    }),
+    table = net.connect(port, host);
+
+    me.on('error', e => {
+        console.error(e);
+        if (!program.ignore)
+            process.exit(1);
+    });
+
+    if (program.debug) {
+        me
+            .on('sent', m => console.log('sent:', m))
+            .on('message', m => console.log('rcvd:', m));
+    }
+
+    me.connect(table);
+    return me;
+}
+
+console.log('making', botCount, 'bots');
+for (let n = 0; n < botCount; ++n) {
+    makeBot(seat);
+    seat = seat.next;
+}
