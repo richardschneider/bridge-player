2a7fbbb36df447545e06007ac8faf8d1bf1dd3ab
Richard Schneider <makaretu@gmail.com>
2016-09-20 16:10:38 +1200

    test(table): just send 'end of session'

--- test/table.js
+++ test/table.js
@@ -1,11 +1,12 @@
 'use strict';
 
 let tms = require('table-master-stream'),
-    bridge = require('bridge.js'),
-    seat = bridge.seat,
     process = require('process'),
     net = require('net');
 
+/*
+ * A simple table that always sends 'end of sesssion' on a connect message.
+ */
 function TestTable(cb) {
     var port = 3000,
         host = 'localhost';
@@ -15,7 +16,7 @@ function TestTable(cb) {
         }
         socket
             .pipe(tms())
-            .on('connect', m => send(`${seat[m.seat].name} "${m.teamName}" seated`));
+            .on('connect', () => send(`end of session`));
     });
     server.listen(port, host, () => {
         process.nextTick(() => {
