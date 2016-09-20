302ac521340f9071793830d293b945e75ed0b781
Richard Schneider <makaretu@gmail.com>
2016-09-20 18:11:25 +1200

    feat(ci): build a 'dist' for browsers

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,14 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var mocha = require('gulp-spawn-mocha');
 var coveralls = require('gulp-coveralls');
+var uglify = require('gulp-uglify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var glob = require('glob');
+var babel = require('babelify');
+var rename = require('gulp-rename');
+var browserify = require('browserify');
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
 
 var DEBUG = process.env.NODE_ENV === 'debug',
     CI = process.env.CI === 'true';
@@ -43,5 +51,43 @@ gulp.task('coverage', function () {
     .pipe(coveralls());
 });
 
+gulp.task('dist-lib', function() {
+    return browserify('./index.js', { standalone: 'BridgePlayer'})
+        .transform(babel, {presets: ['es2015']})
+        .bundle()
+        .pipe(source('bridge-player.js'))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('dist-test', function (cb) {
+  glob('./test/**/*.js', {}, function (err, files) {
+    var b = browserify({standalone: 'spec'});
+    files.forEach(function (file) {
+        b.add(file);
+    });
+    b
+        .transform(babel, {presets: ['es2015']})
+        .bundle()
+        .pipe(source('bridge-player.spec.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/'))
+        .on('end', function() { cb(); });
+  });
+});
+
+gulp.task('test-browser', ['dist'], function () {
+    return gulp.src('test/runner.html')
+        .pipe(mochaPhantomJS({reporter: 'min'}));
+});
+
+gulp.task('dist',    ['dist-lib', 'dist-test']);
+gulp.task('ci',      ['test', 'test-browser', 'dist']);
 gulp.task('test',    ['lint', 'istanbul']);
 gulp.task('default', ['test']);
